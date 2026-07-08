5dcdf176de546424a89b411780eee475e9d0e28f
Gloomsfield <amory@acosta.boo>
2026-07-08 15:23:48 -0700

    setup repo

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+	description = "ttrpg-audio devenv";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+	};
+
+	outputs = { self, nixpkgs }:
+		let
+			lib = nixpkgs.lib;
+
+			forAllSystems = lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
+
+			collapseMultiline = { separator, string }:
+				let
+					splitString = lib.strings.splitString "\n" string;
+					trimString = line: lib.strings.trimWith { start = true; end = true; } line;
+					stringArray = map (line: trimString line) splitString;
+					out = lib.strings.concatStringsSep separator stringArray;
+				in out;
+		in {
+			devShells = forAllSystems (
+				system: let
+					pkgs = import nixpkgs { inherit system; };
+				in {
+					default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+						name = "ttrpg-audio";
+
+						packages = with pkgs; [
+							clang-tools
+							gnumake
+
+							gdb
+						];
+
+						shellHook = let
+							clangdFlags = collapseMultiline {
+								separator = "\n";
+								string = ''
+									-isystem${pkgs.clangStdenv.cc.libc.dev}/include/
+								'';
+							};
+						in ''
+							echo -e "${clangdFlags}" > compile_flags.txt
+						'';
+					};
+				}
+			);
+	};
+}
