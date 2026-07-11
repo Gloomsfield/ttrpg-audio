39ab2030f2733d5b2bd8719fe71e8a6478cc4ec9
Gloomsfield <amory@acosta.boo>
2026-07-10 22:29:21 -0700

    update parser to use file utils

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -4,9 +4,11 @@
 
 #include "component/component.h"
 
+#include "utils/files.h"
+
 typedef struct parsed_file_t parsed_file_t;
 
-bool parse_file_contents(char* file_contents, parsed_file_t* result_file);
+bool parse_file_contents(file_contents_t file_contents, parsed_file_t** result_file);
 
-bool get_code_from_parsed_file(parsed_file_t* file, char** result_code);
-bool get_info_from_parsed_file(parsed_file_t* file, component_info_t* result_info);
+char* get_code_from_parsed_file(parsed_file_t* file);
+component_info_t get_info_from_parsed_file(parsed_file_t* file);
