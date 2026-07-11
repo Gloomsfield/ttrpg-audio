0148629108cc8b50b2d69393e254d273373b9b1e
Gloomsfield <amory@acosta.boo>
2026-07-10 20:05:30 -0700

    rename "str" suffix to "string"

--- src/utils/map.h
+++ src/utils/map.h
@@ -7,10 +7,10 @@
 typedef struct map_t map_t;
 
 void insert_into_map_hash(map_t* map, hash_key_t hashed_key, void* value);
-void insert_into_map_str(map_t* map, const char* key, void* value);
+void insert_into_map_string(map_t* map, const char* key, void* value);
 
 void remove_from_map_hash(map_t* map, hash_key_t hashed_key);
-void remove_from_map_str(map_t* map, const char* key);
+void remove_from_map_string(map_t* map, const char* key);
 
 bool get_from_map_hash(map_t* map, hash_key_t hashed_key, void* result_value);
-bool get_from_map_str(map_t* map, const char* key, void* result_value);
+bool get_from_map_string(map_t* map, const char* key, void* result_value);
