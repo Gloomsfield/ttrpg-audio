fd0369f355550f49a3106fd277ceb2de6641198f
Gloomsfield <amory@acosta.boo>
2026-07-10 20:56:06 -0700

    remove blueprint_t definition from blueprint header

--- src/blueprint/blueprint.h
+++ src/blueprint/blueprint.h
@@ -4,10 +4,8 @@
 
 #include "component/component.h"
 
-typedef struct blueprint_t {
-	
-} blueprint_t;
+typedef struct blueprint_t blueprint_t;
 
 bool create_blueprint(component_info_t info, char* code, blueprint_t* result_blueprint);
 
-bool build_blueprint(blueprint_t blueprint, component_t* result_component);
+bool build_blueprint(blueprint_t* blueprint, component_t* result_component);
