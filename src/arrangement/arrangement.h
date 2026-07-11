016d23cdb645e087deab0e5010dd24ea068b7c83
Gloomsfield <amory@acosta.boo>
2026-07-10 21:46:01 -0700

    update arranger to reflect dependency hierarchy

--- src/arrangement/arrangement.h
+++ src/arrangement/arrangement.h
@@ -4,6 +4,7 @@
 
 #include "blueprint/blueprint.h"
 #include "component/component.h"
+#include "dispatcher/dispatcher.h"
 
 typedef struct arranger_t arranger_t;
 
@@ -12,9 +13,9 @@ typedef struct selection_info_t {
 	uint32_t n;
 } selection_info_t;
 
-bool arranger_init(arranger_t** arranger, void(* on_component_built)(component_t* new_component));
+bool arranger_init(arranger_t** arranger, bool(* on_component_built)(dispatcher_t*, component_t* new_component));
 bool arranger_start(arranger_t* arranger);
 
-void arranger_update_selection(arranger_t* arranger, selection_info_t* new_selection);
+bool arranger_update_selection(arranger_t* arranger, selection_info_t* new_selection);
 
-void arranger_register_blueprint(arranger_t* arranger, blueprint_t* new_blueprint);
+bool arranger_register_blueprint(arranger_t* arranger, blueprint_t* new_blueprint);
