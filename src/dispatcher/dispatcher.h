200f4263f50a876355bfe10b617daeb7f40e5665
Gloomsfield <amory@acosta.boo>
2026-07-10 21:07:08 -0700

    add declarations to dispatcher header

---
+++ src/dispatcher/dispatcher.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdbool.h>
+
+#include "component/component.h"
+
+typedef struct dispatcher_t dispatcher_t;
+
+bool init_dispatcher(dispatcher_t** dispatcher);
+
+bool dispatch_component(dispatcher_t* dispatcher, component_t* component);
