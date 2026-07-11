f3bb6f60115f73dc8f8a992e6972d43217d39d31
Gloomsfield <amory@acosta.boo>
2026-07-10 21:29:55 -0700

    update frontend_init signature

--- src/frontend/frontend.h
+++ src/frontend/frontend.h
@@ -6,6 +6,6 @@
 
 typedef struct frontend_t frontend_t;
 
-bool frontend_init(frontend_t* frontend, void(* on_selection_changed)(arranger_t*, selection_info_t*));
+bool frontend_init(frontend_t** frontend, void(* on_selection_changed)(arranger_t*, selection_info_t*));
 
 bool frontend_start(frontend_t* frontend);
