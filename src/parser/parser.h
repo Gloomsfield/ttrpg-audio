45051f51760d004dee475078155f01a390fb5865
Gloomsfield <amory@acosta.boo>
2026-07-10 20:55:13 -0700

    add declarations to parser header

---
+++ src/parser/parser.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdbool.h>
+
+#include "component/component.h"
+
+typedef struct parsed_file_t parsed_file_t;
+
+bool parse_file(char* path, parsed_file_t* result_file);
+
+bool get_code_from_parsed_file(parsed_file_t* file, char** result_code);
+bool get_info_from_parsed_file(parsed_file_t* file, component_info_t* result_info);
