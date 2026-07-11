f9ae9d42a11843488151cc0bd3758815bcd9ee3e
Gloomsfield <amory@acosta.boo>
2026-07-10 21:45:18 -0700

    replace parse_file declaration with parse_file_contents

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,7 +6,7 @@
 
 typedef struct parsed_file_t parsed_file_t;
 
-bool parse_file(char* path, parsed_file_t* result_file);
+bool parse_file_contents(char* file_contents, parsed_file_t* result_file);
 
 bool get_code_from_parsed_file(parsed_file_t* file, char** result_code);
 bool get_info_from_parsed_file(parsed_file_t* file, component_info_t* result_info);
