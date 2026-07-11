ba4e6e0d2ef5d9711e4030a8ab34e0d0d0c6d0a5
Gloomsfield <amory@acosta.boo>
2026-07-10 20:04:24 -0700

    add stubs to component header

---
+++ src/component/component.h
@@ -0,0 +1,9 @@
+#pragma once
+
+typedef struct component_info_t {
+	const char* name;
+	const char* description;
+	const char** properties;
+} component_info_t;
+
+typedef struct component_t {} component_t;
