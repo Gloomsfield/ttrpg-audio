59c19616a0cb215c52ccc79890b630506dfc6696
Gloomsfield <amory@acosta.boo>
2026-07-10 22:28:37 -0700

    update create_blueprint signature

--- src/blueprint/blueprint.h
+++ src/blueprint/blueprint.h
@@ -6,6 +6,6 @@
 
 typedef struct blueprint_t blueprint_t;
 
-bool create_blueprint(component_info_t info, char* code, blueprint_t* result_blueprint);
+bool create_blueprint(component_info_t info, char* code, blueprint_t** result_blueprint);
 
 bool build_blueprint(blueprint_t* blueprint, component_t* result_component);
